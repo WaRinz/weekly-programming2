1a61a0f98dce41d1c5ac7cf364e691993d290c5b
WaRinz <seleniumdichloride@gmail.com>
2020-08-14 23:23:45 +0700

    update

--- weekly programming2/programming2.cpp
+++ weekly programming2/programming2.cpp
@@ -1,50 +1,27 @@
 #include<stdio.h>
 #include<conio.h>
-void foodMenu(void);
-
-int main(void)
+int main()
 {
-	/*MENU SELECTION*/
-	int menu;
-	printf("-- WELCOME TO CASHIER -- \n");
-	printf("\nSELECT \n\n 1->FOODMENU \n 2->CHECKBILL \n 3->NEW MEMBER \n");
-	scanf_s("%d", &menu);
-		if (menu == 1)
-			{
-			foodMenu();
-			}
-	
-		
-	
-	return 0;
-}
-void foodMenu(void)
-{
-	int home;
-	printf("A0  Sushi Season      300.-\n");
-	printf("A1  Miso Soup          25.-\n");
-	printf("A2  Tempura            80.-\n");
-	printf("A3  Ramen              60.-\n");
-	printf("A4  Salmon Set        250.-\n");
-	printf("A5  Tofu Steak         80.-\n");
-	printf("A6  Salmon Sashimi    100.-\n");
-	printf("A7  Sashimi Set       260.-\n");
-	printf("A8  Oden               90.-\n");
-	printf("A9  Yaki Soba          80.-\n");
-	printf("A10 Natto              50.-\n");
-	printf("A11 Onigiri            60.-\n");
-	printf("A12 Takoyaki           75.-\n");
+	int age;
+	printf("PLEASE ENTER YOUR AGE : ");
+	scanf_s("%d", &age);
 
-	printf("GO TO MENU (YES=1/NO=2) : ");
-	scanf_s("%d", &home);
-	if (home == 1)
-		{
-		main();
-		}
-	else if (home == 2)
-		{
-		printf("\n\n----- THANK YOU! -----\n\n\npress any key to continue");
-		}
+	if (age <= 11)
+	{
+		printf("%d -> You are Kid", age);
+	}
+	else if (age <= 20)
+	{
+		printf("%d -> You are Teen", age);
+	}
+	else if (age <= 60)
+	{
+		printf("%d -> You are Adult", age);
+	}
+	else
+	{
+		printf("%d -> You are Old", age);
+	}
 
 }
 
